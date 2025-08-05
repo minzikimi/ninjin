6d5c865b229ded08f30e83d4013e55e9f88f6b49
minzikimi <minji.kim@hyperisland.se>
2025-08-05 11:51:30 +0900

    set up tailwind css

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,18 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <h1>hi!</h1>
+  <div
+    class="flex flex-col items-center justify-center min-h-screen bg-yellow-50 p-8"
+  >
+    <h1 class="text-3xl font-bold text-orange-600 mb-4">
+      ninjin is carrot in Japanese
+    </h1>
+    <p class="text-lg text-gray-700">
+      Try changing
+      <span class="text-orange-400 font-semibold">karrot</span> classes right
+      here!
+    </p>
+  </div>
 </template>
 
 <style scoped></style>
