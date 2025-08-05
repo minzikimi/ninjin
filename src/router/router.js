958a810e310f6984d0a714334ef674953461e7ee
minzikimi <minji.kim@hyperisland.se>
2025-08-05 12:48:39 +0900

    set up router

---
+++ src/router/router.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "../views/Login.vue";
+import SignUp from "../views/SignUp.vue";
+import Home from "../views/Home.vue";
+import Chat from "../views/Chat.vue";
+import Profile from "../views/Profile.vue";
+import Community from "../views/Community.vue";
+
+const routes =[
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  { path: '/signup', component: SignUp },
+  { path: '/home', component: Home },
+  { path: '/chat', component : Chat},
+  { path: '/community', component : Community},
+  { path: '/profile', component : Profile}
+];
+
+const router = createRouter({
+    history : createWebHistory(),
+    routes,
+})
+
+export default router;
